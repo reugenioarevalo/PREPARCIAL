93bff6293ce7fe1b08a7842e7c0a81c4c3532737
Eugenio <reugenioarevalo@gmail.com>
2020-09-24 22:36:57 -0300

    no compilaa por el error ese

--- parcial.cpp
+++ parcial.cpp
@@ -25,8 +25,9 @@ void promPeso(){}
 
 ///*Listar apellidos y nombres del usuario que más tiempo en promedio haya entrenado.*/
 void mayorTiempoEntrenado(){
-   FILE *p;
-    Rutina reg, *vec;
+//   FILE *p;
+//    Rutina reg;
+    Rutina *vec;
     int *tiemp;
    int  cant_user=0;
     int cant_rutinas=0;
@@ -37,25 +38,31 @@ void mayorTiempoEntrenado(){
     if (tiemp == NULL){return;}
 
     vec = (Rutina *) malloc(sizeof(Rutina) * cant_rutinas);
-    if (vec == NULL){free(tiemp);return; }
+    if (vec == NULL){ free(tiemp); return; }
+//
+//    p=fopen(RUTINAS,"rb");
+//    if(p==NULL){
+//            return  ;
+//    }
+//    fread(&reg,sizeof(Rutina),1,p);
+//    fclose(p);
 
-    p=fopen(RUTINAS,"rb");
-    if(p==NULL){
-            return  ;
-    }
-    fread(&reg,sizeof(Rutina),1,p);
-    fclose(p);
 
+   if(leer_rutina(vec, cant_rutinas) == cant_rutinas){
 
-    if (leer_rutina(vec, cant_rutinas) == cant_rutinas){
-
-    }
     for(int i=0; cant_rutinas; i++){
         if(vec[i].idUsuario != vec[i+1].idUsuario){
             cant_user++;
         }
     }
     cout<<cant_user;
+    }else{
+        cout<<"Hubo un error al reportar.";
+    }
+
+    free(tiemp);
+    free(vec);
+    return;
 }
 
 int leer_rutina(Rutina *vec, int cant){
