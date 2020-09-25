bed08a0ea262190b0ce29498bc70f4bc7fd9e309
Eugenio <reugenioarevalo@gmail.com>
2020-09-24 23:03:30 -0300

    compilo

--- parcial.cpp
+++ parcial.cpp
@@ -18,48 +18,75 @@
 
 using namespace rlutil;
 using namespace std;
-const char *RUTINAS = "archivos/Entrenamientos.dat";
+void vacio(int *v, int t){
+    for(int i=0;i<t; i++){
+        v[i] = 0;
+    }
+}
+Rutina rutXidUs(int idUsuario){
+      FILE *p;
+    int posiciones=0;
+    Rutina reg, err;
 
+    p = fopen("archivos/Entrenamientos.dat", "rb");
+        if(p==NULL){
+            cout << "Error de archivo";
+            return err ;
+        }
+    while(fread(&reg,sizeof (Rutina),1,p)==1){
+            if(reg.idUsuario==idUsuario){
+                return reg;
+            }
+    }
+
+    fclose(p);
+   return err;
+
+}
 ///*Para cada perfil, indicar el promedio de peso.*/
 void promPeso(){}
 
 ///*Listar apellidos y nombres del usuario que más tiempo en promedio haya entrenado.*/
 void mayorTiempoEntrenado(){
-//   FILE *p;
-//    Rutina reg;
+
     Rutina *vec;
+    Rutina rutinaPorUs;
     int *tiemp;
    int  cant_user=0;
     int cant_rutinas=0;
- //  int cantRutXUsuario=1;
+
     cant_rutinas=contarRutinas();
 
     tiemp = (int *) malloc(sizeof(int) * cant_rutinas);
-    if (tiemp == NULL){return;}
+    if (tiemp == NULL){ return;}
+
 
     vec = (Rutina *) malloc(sizeof(Rutina) * cant_rutinas);
     if (vec == NULL){ free(tiemp); return; }
-//
-//    p=fopen(RUTINAS,"rb");
-//    if(p==NULL){
-//            return  ;
-//    }
-//    fread(&reg,sizeof(Rutina),1,p);
-//    fclose(p);
 
 
    if(leer_rutina(vec, cant_rutinas) == cant_rutinas){
-
-    for(int i=0; cant_rutinas; i++){
+    for(int i=0; i< cant_rutinas; i++){
         if(vec[i].idUsuario != vec[i+1].idUsuario){
             cant_user++;
         }
     }
-    cout<<cant_user;
-    }else{
-        cout<<"Hubo un error al reportar.";
+    vacio(tiemp, cant_user );
+    for(int i=0; i< cant_user; i++){
+            for(int j=0; j< cant_rutinas; j++){
+                rutinaPorUs= rutXidUs(vec[i].idUsuario);
+                if(vec[i].idUsuario==rutinaPorUs.idUsuario){
+                    tiemp[i]+=rutinaPorUs.tiempo;
+                }
+            }
+        cout<<tiemp[i]<<endl;
+        }
     }
 
+    else{
+        cout<<"Hubo un error al reportar.";
+    }
+    anykey();
     free(tiemp);
     free(vec);
     return;
@@ -67,7 +94,7 @@ void mayorTiempoEntrenado(){
 
 int leer_rutina(Rutina *vec, int cant){
     int c;
-    FILE *p = fopen(RUTINAS, "rb");
+    FILE *p = fopen("archivos/Entrenamientos.dat", "rb");
     if (p == NULL){
         return 0;
     }
@@ -75,19 +102,3 @@ int leer_rutina(Rutina *vec, int cant){
     fclose(p);
     return c;
 }
-
-/**
-float *kgs;
-    Participante *vec;
-    int cant_participantes = cantidad_participantes();
-    kgs = (float *) malloc(sizeof(float) * cant_participantes);
-    if (kgs == NULL){
-        return;
-    }
-    vec = (Participante *) malloc(sizeof(Participante) * cant_participantes);
-    if (vec == NULL){
-        free(kgs);
-        return;
-    }
-    poner0(kgs, cant_participantes);
-    */
